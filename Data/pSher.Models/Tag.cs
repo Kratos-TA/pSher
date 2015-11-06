d5da50e96b6192abd9dd4f4c234c6aa32ecbfb45
Ivo Paounov <ivo.paunov@gmail.com>
2015-11-06 10:56:39 +0200

    PSher.Models.Tag - added name;

--- Data/pSher.Models/Tag.cs
+++ Data/pSher.Models/Tag.cs
@@ -15,6 +15,11 @@
         [Key]
         public int Id { get; set; }
 
+        [Required]
+        [MinLength(3)]
+        [MaxLength(64)]
+        public string Name { get; set; }      
+
         public virtual ICollection<Image> Images
         {
             get { return this.images; }
