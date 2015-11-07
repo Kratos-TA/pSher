140634ccfc34914fe4589b13afbc3a5066154db3
Ivo Paounov <ivo.paunov@gmail.com>
2015-11-07 11:13:55 +0200

    PSher.Api - Account Models fixed to work with PSher.Models

--- Server/pSher.Api/App_Start/Startup.Auth.cs
+++ Server/pSher.Api/App_Start/Startup.Auth.cs
@@ -10,6 +10,7 @@ using Microsoft.Owin.Security.OAuth;
 using Owin;
 using PSher.Api.Providers;
 using PSher.Api.Models;
+using PSher.Data;
 
 namespace PSher.Api
 {
@@ -23,7 +24,7 @@ namespace PSher.Api
         public void ConfigureAuth(IAppBuilder app)
         {
             // Configure the db context and user manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext(PSherDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
 
             // Enable the application to use a cookie to store information for the signed in user
--- Server/pSher.Api/Providers/ApplicationOAuthProvider.cs
+++ Server/pSher.Api/Providers/ApplicationOAuthProvider.cs
@@ -10,6 +10,7 @@ using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
 using PSher.Api.Models;
+using PSher.Models;
 
 namespace PSher.Api.Providers
 {
@@ -31,7 +32,7 @@ namespace PSher.Api.Providers
         {
             var userManager = context.OwinContext.GetUserManager<ApplicationUserManager>();
 
-            ApplicationUser user = await userManager.FindAsync(context.UserName, context.Password);
+            User user = await userManager.FindAsync(context.UserName, context.Password);
 
             if (user == null)
             {
