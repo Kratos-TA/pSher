a8d621ffa693b40e5d5f154b12d62a5e6d4852ef
veselints <veselintsvetanov@gmail.com>
2015-11-08 12:10:16 +0200

    Updated slider to be responsive, depending on the screen aspect ration

--- JsClient/scripts/try.js
+++ JsClient/scripts/try.js
@@ -1,20 +1,22 @@
 jQuery(document).ready(function($) {
+    var slideWidth = '100vw';
+    var slideHeight = '100vh';
+    var sliderUlWidth = '400vw';
+    var windowWidth = $(window).width();
 
     $(window).resize(function() {
         windowWidth = $(window).width();
         $('#slider ul').css({
             marginLeft: -windowWidth
         });
+        selectSliderImages(windowWidth);
     });
 
     setInterval(function() {
         moveRight();
     }, 7000);
 
-    var slideWidth = '100vw';
-    var slideHeight = '100vh';
-    var sliderUlWidth = '400vw';
-    var windowWidth = $(window).width();
+    selectSliderImages(windowWidth);
 
     $('#slider').css({
         width: slideWidth,
@@ -32,6 +34,31 @@ jQuery(document).ready(function($) {
     });
 
     $('#slider ul li:last-child').prependTo('#slider ul');
+
+    function selectSliderImages(windowWidth) {
+        var windowHeight = $(window).height();
+        var aspectRatio = windowWidth / windowHeight;
+        var $slide1 = $("#slide1");
+        var $slide2 = $("#slide2");
+        var $slide3 = $("#slide3");
+        var $slide4 = $("#slide4");
+        var imagePath;
+
+        if (aspectRatio <= 1) {
+            imagePath = "0_75";
+        } else if (aspectRatio <= 1.70) {
+            imagePath = "1_33";
+        } else if (aspectRatio <= 2.25) {
+            imagePath = "1_75";
+        } else {
+            imagePath = "2_30";
+        }
+
+        $slide1.css('background-image', 'url(' + './images/' + imagePath + '/image1.jpg' + ')');
+        $slide2.css('background-image', 'url(' + './images/' + imagePath + '/image2.jpg' + ')');
+        $slide3.css('background-image', 'url(' + './images/' + imagePath + '/image3.jpg' + ')');
+        $slide4.css('background-image', 'url(' + './images/' + imagePath + '/image4.jpg' + ')');
+    }
 
     function moveLeft() {
         $('#slider ul').animate({
