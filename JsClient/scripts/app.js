f6a8547756bf251d7d69d7d3e670f7ba0a0cff94
veselints <veselintsvetanov@gmail.com>
2015-11-19 22:32:10 +0200

    Plugged PuNub to listen for notifications. The notification pops up as alert div. At the moment we are not using the "send" function (even if it is included). You can find everything related at the beginning of the app.js file.

--- JsClient/scripts/app.js
+++ JsClient/scripts/app.js
@@ -24,12 +24,41 @@ import {
 }
 from './controllers/imagesController.js';
 
+import {
+    alertHelper
+}
+from './helpers/alertHelper.js';
+
 var appInitialize = (function() {
     /* use strict */
 
     // Checking if the module is successfully loaded
     console.log('App loaded successfully!');
 
+
+    // Subscribe here to PubNub
+    var pubnub = PUBNUB.init({
+        publish_key: 'pub-c-8e80f845-211b-439e-9f31-bf15f247d4a3',
+        subscribe_key: 'sub-c-e5b4d678-8c6b-11e5-bf00-02ee2ddab7fe'
+    });
+
+    pubnub.subscribe({
+        channel: 'pSher',
+        message: function(mess) {
+            alertHelper.getOkAlert(mess);
+        }
+    });
+
+    // I dont know where is supposed to use this
+    // Should be implemented like that but really what should users publish to PubNub
+    // pubnub.publish({
+    //     channel: 'pSher',
+    //     // message: {
+    //     //     "color": "blue"
+    //     // }
+    // });
+
+    // Check if user is logged
     if (localStorage.USERNAME_KEY && localStorage.AUTHENTICATION_KEY) {
         $('#log').attr('href', '#/logout');
         $('#log').html('Logout');
